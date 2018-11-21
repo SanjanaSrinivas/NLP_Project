1a03a4935bcfcc699f0ea543d893534a4315367f
Shyam Gaurav Venkatesh <shyamgav@usc.edu>
2018-11-21 13:00:47 -0800

    add baseline classifiers

--- python/baseline_BOW_classifiers.py
+++ python/baseline_BOW_classifiers.py
@@ -3,11 +3,12 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-
-df = pd.read_csv('data/600_input.csv', header = 0)
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+df = pd.read_csv('data/clean_data.csv', header = 0)
 data_matrix = df.as_matrix()
 
-text_data, labels = data_matrix.T
+_, text_data, labels = data_matrix.T
 count_vect = CountVectorizer(encoding = "ISO-8859-1")
 count_data = count_vect.fit_transform(text_data)
 
@@ -16,6 +17,15 @@ X_train, X_test, y_train, y_test = train_test_split(count_data, labels,
 
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
+print "Multinomial NB"
+print clf.score(X_test, y_test)
 
-print clf.predict(X_test)
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+print "Logistic Regression"
+print clf.score(X_test, y_test)
+
+clf = SVC()
+clf.fit(X_train, y_train)
+print "SVM"
 print clf.score(X_test, y_test)
