0fcb7b55b094d9d0de28b722d6aee85f931f71fd
Shyam Gaurav Venkatesh <shyamgav@usc.edu>
2018-11-21 16:13:13 -0800

    add word2vec embedding

--- python/baseline_BOW_classifiers.py
+++ python/baseline_BOW_classifiers.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report
+from nltk.tokenize import RegexpTokenizer
+import gensim
 
 # RUN USING python -W ignore flag
 
@@ -72,6 +74,47 @@ print "Multinomial NB"
 y_pred = clf.predict(X_test)
 accuracy, precision, recall, f1 = get_metrics(y_test, y_pred)
 print("accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision, recall, f1))
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+print "Logistic Regression"
+y_pred = clf.predict(X_test)
+accuracy, precision, recall, f1 = get_metrics(y_test, y_pred)
+print("accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision, recall, f1))
+
+clf = SVC()
+clf.fit(X_train, y_train)
+print "SVM"
+y_pred = clf.predict(X_test)
+accuracy, precision, recall, f1 = get_metrics(y_test, y_pred)
+print("accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision, recall, f1))
+
+print "\nWord2Vec"
+word2vec_path = "GoogleNews-vectors-negative300.bin.gz"
+word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
+
+def get_average_word2vec(tokens_list, vector, generate_missing=False, k=300):
+    if len(tokens_list)<1:
+        return np.zeros(k)
+    if generate_missing:
+        vectorized = [vector[word] if word in vector else np.random.rand(k) for word in tokens_list]
+    else:
+        vectorized = [vector[word] if word in vector else np.zeros(k) for word in tokens_list]
+    length = len(vectorized)
+    summed = np.sum(vectorized, axis=0)
+    averaged = np.divide(summed, length)
+    return averaged
+
+def get_word2vec_embeddings(vectors, clean_questions, generate_missing=False):
+    embeddings = clean_questions['tokens'].apply(lambda x: get_average_word2vec(x, vectors, generate_missing=generate_missing))
+    return list(embeddings)
+
+clean_questions = pd.read_csv("data/clean_data.csv")
+tokenizer = RegexpTokenizer(r'\w+')
+clean_questions["tokens"] = clean_questions["text"].apply(tokenizer.tokenize)
+
+embeddings = get_word2vec_embeddings(word2vec, clean_questions)
+X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.2, random_state=40)
 
 clf = LogisticRegression()
 clf.fit(X_train, y_train)
