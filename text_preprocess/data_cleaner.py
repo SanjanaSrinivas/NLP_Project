12c7804f970c9dc85fbdc8a9819426a031f75e36
Shyam Gaurav Venkatesh <shyamgav@usc.edu>
2018-11-21 17:45:14 -0800

    Update total and clean data

--- text_preprocess/data_cleaner.py
+++ text_preprocess/data_cleaner.py
@@ -3,40 +3,39 @@ import pandas as pd
 import numpy as np
 import re
 import codecs
+import csv
 from nltk.tokenize import RegexpTokenizer
 import matplotlib.pyplot as plt
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 
-input_file = codecs.open("data/socialmedia_relevant_cols.csv", "r",encoding='utf-8', errors='replace')
-output_file = open("data/socialmedia_relevant_cols_clean.csv", "w")
+input_file = codecs.open("data/total_labelled.csv", "r",encoding='utf-8')
+output_file = open("data/total_cleaned.csv", "w")
 
 def sanitize_characters(raw, clean):    
     for line in input_file:
         out = line.encode("utf-8").strip().replace("#", "")
         output_file.write(out + '\n')
 
-sanitize_characters(input_file, output_file)
-questions = pd.read_csv("data/socialmedia_relevant_cols_clean.csv")
-questions.columns=['text', 'choose_one', 'class_label']
-
 def standardize_text(df, text_field):
     df[text_field] = df[text_field].str.replace(r"http\S+", "")
-    df[text_field] = df[text_field].str.replace(r"https\S+", "")
     df[text_field] = df[text_field].str.replace(r"http", "")
-    df[text_field] = df[text_field].str.replace(r"https", "")
     df[text_field] = df[text_field].str.replace(r"@\S+", "")
-    df[text_field] = df[text_field].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+    df[text_field] = df[text_field].str.replace(r"[^A-Za-z0-9(),!?@\_\n]", " ")
     df[text_field] = df[text_field].str.replace(r"@", "at")
     df[text_field] = df[text_field].str.lower()
     return df
 
+sanitize_characters(input_file, output_file)
+questions = pd.read_csv("data/total_cleaned.csv")
+questions.columns=['text', 'label']
+
 questions = standardize_text(questions, "text")
-questions.to_csv("data/clean_data.csv")
+questions.to_csv("data/clean_data.csv", index=False)
 clean_questions = pd.read_csv("data/clean_data.csv")
 
-print clean_questions.groupby("class_label").count()
+#print clean_questions.groupby("class_label").count()
 
 clean_questions = pd.read_csv("data/clean_data.csv")
 tokenizer = RegexpTokenizer(r'\w+')
